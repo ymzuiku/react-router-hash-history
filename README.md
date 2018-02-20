63c607968a78ae0c8971dd67d79a7e6d113388e7
ymzuiku <ymzuiku@gmail.com>
2018-02-20 15:36:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# React Router Hash History
+
+![](markdownimg/2018-02-20-15-34-33.png)
+
+## How to use
+
+Create two componen
+```
+class Desktop extends React.Component{
+  render(){
+    return <View>
+      <Text>Desktop</Text>
+    </View>
+  }
+}
+class Discover extends React.Component{
+  render(){
+    return <View>
+      <Text>Discover</Text>
+    </View>
+  }
+}
+```
+
+```
+// in react or react-native App.js use the components
+import {Router, Route, history, Switch,Redirect,hashChange} from 'react-router-hash-history'
+class App extends RX.Component {
+  componentDidMount(){
+    // if begin use other URL page, to be use hashChange()
+    hashChange()
+  }
+  render() {
+    return (
+      <Router history={history} >
+        <View style={[ss.box]} >
+          <Text onPress={()=>{
+            // changeURL and Router
+            history.push('/Discover/')
+          }} style={[ss.text]} >Go to Discover</Text>
+          <Text  onPress={()=>{
+            // changeURL and Router
+            history.push('/Desktop/')
+          }} style={[ss.text]} >Go to Desktop</Text>
+          <Switch >
+          <Route exact path='/Discover/*' component={Discover} ></Route>
+          <Route exact path='/Desktop/*' component={Desktop} ></Route>
+          </Switch>
+        </View>
+      </Router>
+    )
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+
+let {createMemoryHistory} = require('history')
+let {Route, Router, Prompt, Redirect, Switch} = require('react-router')
+
+let position = '#'
+let history = createMemoryHistory()
+let lastHistory = {pathname: "/", search: "", hash: "", state: undefined, key: ''}
+history.listen((e) => {
+  try {
+    if(lastHistory.key !== e.key){
+      lastHistory = {pathname: e.pathname, search: e.search, hash: e.hash, state: e.state, key: e.key}
+      if (window.location) {
+        window.location.href = position + e.pathname
+      }
+    }
+  } catch (err) { }
+})
+
+//监听触发操作  
+function hashChange() {
+  let pathname = window.location.href.split(position)[1]
+  if(pathname !== lastHistory.pathname){
+    history.push(pathname)
+  }
+}
+
+try {
+  //url变化监听器  
+  if (('onhashchange' in window) && ((typeof document.documentMode === 'undefined') || document.documentMode == 8)) {
+    // 浏览器支持onhashchange事件  
+    // window.onhashchange = hashChange;  // TODO，对应新的hash执行的操作函数  
+  } 
+  else {
+    let oldHash = window.location.hash
+    function isHashChanged(){
+      return oldHash === window.location.hash
+    }
+    // 不支持则用定时器检测的办法  
+    setInterval(function () {
+      // 检测hash值或其中某一段是否更改的函数， 在低版本的iE浏览器中通过window.location.hash取出的指和其它的浏览器不同，要注意  
+      var ischanged = isHashChanged();
+      if (ischanged) {
+        hashChange();  // TODO，对应新的hash执行的操作函数  
+      }
+    }, 350); 
+  }
+} catch (err) { }
+
+const ReactRouterHistory = {
+  history, Route, Router, Prompt, Redirect, Switch, lastHistory, hashChange,position
+}
+module.exports = ReactRouterHistory
